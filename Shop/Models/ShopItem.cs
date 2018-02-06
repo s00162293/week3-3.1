27bf05907d0c32ef48248c59a295fb65920faa48
S00162293 <s00162293@mail.itsligo.ie>
2018-02-06 13:42:03 +0000

    classes added

--- Shop/Models/ShopItem.cs
+++ Shop/Models/ShopItem.cs
@@ -5,7 +5,18 @@ using System.Web;
 
 namespace Shop.Models
 {
+    public enum ItemClass
+    {
+        Heath,
+        Magic,
+        Stamina
+    }
     public class ShopItem
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public ItemClass Class { get; set; }
+        public string ImageURL { get; set; }
     }
 }
---
+++ Shop/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public double Balance { get; set; }
+
+        public virtual ICollection<Purchases> Purchases{ get; set; }
+    }
+}
